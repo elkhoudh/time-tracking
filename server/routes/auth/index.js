fd2c29c88101f737c9f89ee9f700271495d433e2
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-05-15 13:19:29 -0400

    Boilerplate setup for the backend

---
+++ server/routes/auth/index.js
@@ -0,0 +1,33 @@
+const route = require("express").Router();
+const models = require("../../common/helpers");
+const errorHelper = require("../../error-helper/errorHelper");
+
+// @route    /api/auth/
+// @desc     POST signing up user
+// @Access   Public
+route.post("/", async (req, res) => {
+  const { name, email, image_url, nickname, sub } = req.body;
+
+  if (!name || !email || !image_url || !nickname || !sub) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  try {
+    const exists = await models.findBy("users", { email, sub });
+
+    if (exists) {
+      return res.status(200).json(exists);
+    }
+
+    const [id] = await models.add("users", req.body);
+
+    if (id) {
+      const user = await models.findBy("users", { id });
+      return res.status(200).json(user);
+    }
+  } catch (error) {
+    return errorHelper(500, error, res);
+  }
+});
+
+module.exports = route;
